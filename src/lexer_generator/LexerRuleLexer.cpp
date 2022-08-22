a28d63e8cf2f2d49a6bab035d508fceab87d2b64
Creepsy <Robin.gd@outlook.de>
2022-08-22 12:14:32 +0200

    finished LexerRuleLexer implementation

--- src/lexer_generator/LexerRuleLexer.cpp
+++ src/lexer_generator/LexerRuleLexer.cpp
@@ -1,11 +1,14 @@
 #include "LexerRuleLexer.h"
 
+#include <stdexcept>
+
 #include "util/encoding.h"
 
 using namespace lexer_generator;
 
 const std::map<Token::TokenType, std::string> lexer_generator::Token::TYPE_NAMES = {
     {Token::TokenType::UNDEFINED, "undefined"},
+    {Token::TokenType::IGNORE, "ignore"},
     {Token::TokenType::END_OF_FILE, "eof"},
     {Token::TokenType::IDENTIFER, "identifier"},
     {Token::TokenType::EQUALS, "eq"},
@@ -13,7 +16,30 @@ const std::map<Token::TokenType, std::string> lexer_generator::Token::TYPE_NAMES
     {Token::TokenType::END_OF_LINE, "eol"}
 };
 
-lexer_generator::LexerRuleLexer::LexerRuleLexer(std::istream& input) : input(input) {
+
+
+FilePosition& lexer_generator::FilePosition::advance(const char32_t c) {
+    if(c == '\n') {
+        this->line++;
+        this->column = 1;
+    } else {
+        this->column++;
+    }
+
+    return *this;
+}
+
+FilePosition& lexer_generator::FilePosition::advance(const std::u32string& text) {
+    for(const char32_t c : text) {
+        this->advance(c);
+    }
+
+    return *this;
+}
+
+
+
+lexer_generator::LexerRuleLexer::LexerRuleLexer(std::istream& input) : input(input), curr_pos{}, has_buffered_char(false) {
 }
 
 
@@ -21,7 +47,36 @@ lexer_generator::LexerRuleLexer::LexerRuleLexer(std::istream& input) : input(inp
 //public
 
 Token lexer_generator::LexerRuleLexer::next_token() {
-    //TODO
+    Token::TokenType type = Token::TokenType::UNDEFINED;
+    std::u32string identifier;
+
+    this->consume_wspace();
+    FilePosition start = this->curr_pos;
+    if(!this->input.fail()) {
+        start.column--; //remove buffered char from position
+    } else {
+        return Token{Token::TokenType::END_OF_FILE, {}, start, start};
+    }
+
+    char32_t next_char = this->next_char(); //char already buffered, so we can skip eof check
+    identifier += next_char;
+
+    if(next_char == '=') {
+        type = Token::TokenType::EQUALS;
+    } else if(next_char == ';') {
+        type = Token::TokenType::END_OF_LINE;
+    } else if(next_char == '$') {
+        type = Token::TokenType::IGNORE;
+    } else if(next_char == '"') {
+        if(this->get_regex(identifier)) type = Token::TokenType::REGEX;
+        
+    } else if(std::isalpha(next_char) | next_char == '_') {
+        if(this->get_identifier(identifier)) type = Token::TokenType::IDENTIFER; 
+    }
+
+    FilePosition end = FilePosition{start}.advance(identifier);
+
+    return Token{type, identifier, start, end};
 }
 
 
@@ -29,14 +84,83 @@ Token lexer_generator::LexerRuleLexer::next_token() {
 //private
 
 char32_t lexer_generator::LexerRuleLexer::next_char() {
-    char32_t next = encoding::get_utf8(this->input);
+    if(this->has_buffered_char) {
+        this->has_buffered_char = false;
 
-    if(next == '\n') {
-        this->curr_pos.line++;
-        this->curr_pos.column = 0;
-    } else {
-        this->curr_pos.column++;
+        return this->buffer;
     }
 
+    char32_t next = encoding::get_utf8(this->input);
+    if(this->input.good()) this->curr_pos.advance(next);
+
     return next;
+}
+
+char32_t lexer_generator::LexerRuleLexer::consume_wspace() {
+    char32_t next;
+
+    do {
+        next = this->next_char();
+    } while(std::iswspace(next) && this->input.good());
+
+    this->insert_in_buffer(next);
+
+    return next;
+}
+
+bool lexer_generator::LexerRuleLexer::get_regex(std::u32string& output) {
+    while(this->input.good()) {
+        char32_t next = this->next_char();
+        output += next;
+
+        if(next == '"') {
+            break;
+        } else if(next == '\\') {
+            output += this->next_char();
+        } else if(next == '\n') {
+            return false;
+        }
+    }
+
+    if(this->input.eof()) {
+        output.pop_back(); //remove eof char
+        
+        return false;
+    }
+
+    return true;
+}
+
+bool lexer_generator::LexerRuleLexer::get_identifier(std::u32string& output) {
+    while(this->input.good()) {
+        char32_t next = this->next_char();
+
+        if(std::isalnum(next) || next == '_') {
+            output += next;
+        } else {
+            this->insert_in_buffer(next); //char is part of the next token
+            break;
+        }
+    }
+
+    return true;
+}
+
+void lexer_generator::LexerRuleLexer::insert_in_buffer(char32_t to_insert) {
+    if(this->has_buffered_char) throw std::runtime_error("Tried to insert char into buffer, but it is already occupied!");
+
+    this->has_buffered_char = true;
+    this->buffer = to_insert;
+}
+
+
+
+//namespace functions
+
+std::ostream& lexer_generator::operator<<(std::ostream& stream, const FilePosition& to_print) {
+    return stream << "[Ln " << to_print.line << ", Col " << to_print.column << "]";
+}
+
+std::ostream& lexer_generator::operator<<(std::ostream& stream, const Token& to_print) {
+    return stream << to_print.start << "-" << to_print.end << " (" << Token::TYPE_NAMES.at(to_print.type) << ") " << to_print.identifier;
 }
--- src/lexer_generator/LexerRuleLexer.h
+++ src/lexer_generator/LexerRuleLexer.h
@@ -1,20 +1,25 @@
 #pragma once
 
 #include <istream>
+#include <ostream>
 #include <string>
 #include <cstddef>
 #include <map>
 
 namespace lexer_generator {
     struct FilePosition {
-        size_t line;
-        size_t column;
+        size_t line = 1;
+        size_t column = 1;
+
+        FilePosition& advance(const char32_t c);
+        FilePosition& advance(const std::u32string& text);
     };
     
     struct Token {
         enum class TokenType {
             UNDEFINED,
             END_OF_FILE,
+            IGNORE,
             IDENTIFER,
             EQUALS,
             REGEX,
@@ -34,11 +39,23 @@ namespace lexer_generator {
         private:
             std::istream& input;
 
+            bool has_buffered_char;
+            char32_t buffer;
+
             FilePosition curr_pos;
 
             char32_t next_char();
+            char32_t consume_wspace();
+            
+            bool get_regex(std::u32string& output);
+            bool get_identifier(std::u32string& output);
+
+            void insert_in_buffer(char32_t to_insert);
         public:
             LexerRuleLexer(std::istream& input);
             Token next_token();
     };
+
+    std::ostream& operator<<(std::ostream& stream, const FilePosition& to_print);
+    std::ostream& operator<<(std::ostream& stream, const Token& to_print);
 }
