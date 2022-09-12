1d9747a600bf3ab2986984e2497b7642ba6dbb8a
Creepsy <Robin.gd@outlook.de>
2022-09-12 16:16:51 +0200

    added automaton tests

--- tests/util_test.cpp
+++ tests/util_test.cpp
@@ -1,18 +1,26 @@
 #include <sstream>
+#include <stdexcept>
 
 #include "util/unicode.h"
+#include "util/Automaton.h"
 
 #include "TestReport.h"
+#include "TestUtils.h"
 
 bool test_unicode_input();
 bool test_unicode_output();
+
+bool test_automaton_add_states();
+bool test_automaton_add_connections();
 
 int main() {
     tests::TestReport report;
 
     report.add_test("unicode_input", test_unicode_input);
     report.add_test("unicode_output", test_unicode_output);
-
+    
+    report.add_test("automaton_add_states", test_automaton_add_states);
+    report.add_test("automaton_add_connections", test_automaton_add_connections);
 
     report.run();
 
@@ -40,4 +48,28 @@ bool test_unicode_output() {
     output << to_print;
 
     return output.str() == "aШᢶ𐅄";
+}
+
+bool test_automaton_add_states() {
+    sm::Automaton<int, int> test_automaton;
+
+    TEST_TRUE(test_automaton.add_state(10) == 0);
+    TEST_TRUE(test_automaton.add_state(11) == 1);
+
+    return true;
+}
+
+bool test_automaton_add_connections() {
+    sm::Automaton<int, int> test_automaton;
+    
+    sm::Automaton<int, int>::StateID_t first = test_automaton.add_state(10);
+    sm::Automaton<int, int>::StateID_t second = test_automaton.add_state(11);
+
+    TEST_EXCEPT(test_automaton.connect_states(1, 2), std::out_of_range);
+    TEST_EXCEPT(test_automaton.connect_states(2, 0, 10), std::out_of_range);
+
+    TEST_TRUE(test_automaton.connect_states(0, 1) == 0);
+    TEST_TRUE(test_automaton.connect_states(1, 0, 10) == 1);
+
+    return true;
 }
