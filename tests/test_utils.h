ec591eebedb45419e706bca30427e43fa325be9c
Creepsy <Robin.gd@outlook.de>
2022-09-24 10:01:46 +0200

    improved info on test fail

--- tests/test_utils.h
+++ tests/test_utils.h
@@ -1,6 +1,25 @@
 #pragma once
 
-#define TEST_TRUE(value) if(!(value)) return false;
-#define TEST_FALSE(value) if(value) return false;
+#include <iostream>
 
-#define TEST_EXCEPT(code, exception) try { code; return false; } catch(const exception& e) {} catch(...) { return false; }
+#define TEST_TRUE(value) \
+if(!(value)) { \
+    std::cerr << "TEST_TRUE failed on case: '" << #value << "' "; \
+    return false; \
+}   
+
+#define TEST_FALSE(value) \
+if(value) { \
+    std::cerr << "TEST_FALSE failed on case: '" << #value << "' "; \
+    return false; \
+} 
+
+#define TEST_EXCEPT(code, exception) \
+try { \
+    code; \
+    std::cerr << "TEST_EXCEPT failed on case: '" << #code << "' threw no exception (expected " << #exception << ") "; \
+    return false; \
+} catch(const exception& e) {} catch(...) { \
+    std::cerr << "TEST_EXCEPT failed on case: '" << #code << "' threw the wrong exception (expected " << #exception << ") "; \
+    return false; \
+}
