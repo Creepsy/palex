048efa052f327060fe440c783982477a346883d8
Creepsy <Robin.gd@outlook.de>
2023-08-12 09:50:28 +0200

    changed lexer test to handwritten one

--- tests/input/palex_rule_lexer_test.cpp
+++ tests/input/palex_rule_lexer_test.cpp
@@ -1,44 +1,44 @@
 #include <sstream>
 
-#include "PalexRuleLexer.h"
+#include "bootstrap/BootstrapLexer.h"
 
 #include "../test_utils.h"
 
 int main() {
-    std::stringstream input(
+    const char* input =
         "!WSPACE = \"\\s+\";\n"
         "<7>ANOTHER_1 = \"\";\n"
         "$S = some_prod;\n"
         "some_prod = ANOTHER_1 some_prod;\n"
         "some_prod = ;\n"
-    );
-    input::PalexRuleLexer lexer(input);
+        ;
+    bootstrap::BootstrapLexer lexer(input);
 
-    TEST_TRUE(lexer.next_unignored_token().type == input::Token::TokenType::IGNORE)
-    TEST_TRUE(lexer.next_unignored_token().type == input::Token::TokenType::TOKEN)
-    TEST_TRUE(lexer.next_unignored_token().type == input::Token::TokenType::EQ)
-    TEST_TRUE(lexer.next_unignored_token().type == input::Token::TokenType::REGEX)
-    TEST_TRUE(lexer.next_unignored_token().type == input::Token::TokenType::EOL)
+    TEST_TRUE(lexer.next_unignored_token() == bootstrap::TokenInfo::TokenType::IGNORE)
+    TEST_TRUE(lexer.next_unignored_token() == bootstrap::TokenInfo::TokenType::TOKEN)
+    TEST_TRUE(lexer.next_unignored_token() == bootstrap::TokenInfo::TokenType::EQ)
+    TEST_TRUE(lexer.next_unignored_token() == bootstrap::TokenInfo::TokenType::REGEX)
+    TEST_TRUE(lexer.next_unignored_token() == bootstrap::TokenInfo::TokenType::EOL)
 
-    TEST_TRUE(lexer.next_unignored_token().type == input::Token::TokenType::PRIORITY_TAG)
-    TEST_TRUE(lexer.next_unignored_token().type == input::Token::TokenType::TOKEN)
-    TEST_TRUE(lexer.next_unignored_token().type == input::Token::TokenType::EQ)
-    TEST_TRUE(lexer.next_unignored_token().type == input::Token::TokenType::REGEX)
-    TEST_TRUE(lexer.next_unignored_token().type == input::Token::TokenType::EOL)
+    TEST_TRUE(lexer.next_unignored_token() == bootstrap::TokenInfo::TokenType::PRIORITY_TAG)
+    TEST_TRUE(lexer.next_unignored_token() == bootstrap::TokenInfo::TokenType::TOKEN)
+    TEST_TRUE(lexer.next_unignored_token() == bootstrap::TokenInfo::TokenType::EQ)
+    TEST_TRUE(lexer.next_unignored_token() == bootstrap::TokenInfo::TokenType::REGEX)
+    TEST_TRUE(lexer.next_unignored_token() == bootstrap::TokenInfo::TokenType::EOL)
 
-    TEST_TRUE(lexer.next_unignored_token().type == input::Token::TokenType::ENTRY_PRODUCTION)
-    TEST_TRUE(lexer.next_unignored_token().type == input::Token::TokenType::EQ)
-    TEST_TRUE(lexer.next_unignored_token().type == input::Token::TokenType::PRODUCTION)
-    TEST_TRUE(lexer.next_unignored_token().type == input::Token::TokenType::EOL)
+    TEST_TRUE(lexer.next_unignored_token() == bootstrap::TokenInfo::TokenType::ENTRY_PRODUCTION)
+    TEST_TRUE(lexer.next_unignored_token() == bootstrap::TokenInfo::TokenType::EQ)
+    TEST_TRUE(lexer.next_unignored_token() == bootstrap::TokenInfo::TokenType::PRODUCTION)
+    TEST_TRUE(lexer.next_unignored_token() == bootstrap::TokenInfo::TokenType::EOL)
 
-    TEST_TRUE(lexer.next_unignored_token().type == input::Token::TokenType::PRODUCTION)
-    TEST_TRUE(lexer.next_unignored_token().type == input::Token::TokenType::EQ)
-    TEST_TRUE(lexer.next_unignored_token().type == input::Token::TokenType::TOKEN)
-    TEST_TRUE(lexer.next_unignored_token().type == input::Token::TokenType::PRODUCTION)
-    TEST_TRUE(lexer.next_unignored_token().type == input::Token::TokenType::EOL)
+    TEST_TRUE(lexer.next_unignored_token() == bootstrap::TokenInfo::TokenType::PRODUCTION)
+    TEST_TRUE(lexer.next_unignored_token() == bootstrap::TokenInfo::TokenType::EQ)
+    TEST_TRUE(lexer.next_unignored_token() == bootstrap::TokenInfo::TokenType::TOKEN)
+    TEST_TRUE(lexer.next_unignored_token() == bootstrap::TokenInfo::TokenType::PRODUCTION)
+    TEST_TRUE(lexer.next_unignored_token() == bootstrap::TokenInfo::TokenType::EOL)
 
-    TEST_TRUE(lexer.next_unignored_token().type == input::Token::TokenType::PRODUCTION)
-    TEST_TRUE(lexer.next_unignored_token().type == input::Token::TokenType::EQ)
-    TEST_TRUE(lexer.next_unignored_token().type == input::Token::TokenType::EOL)
+    TEST_TRUE(lexer.next_unignored_token() == bootstrap::TokenInfo::TokenType::PRODUCTION)
+    TEST_TRUE(lexer.next_unignored_token() == bootstrap::TokenInfo::TokenType::EQ)
+    TEST_TRUE(lexer.next_unignored_token() == bootstrap::TokenInfo::TokenType::EOL)
     return 0;
 }
